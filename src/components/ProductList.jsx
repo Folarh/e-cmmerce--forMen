5b7058ca5e2ec57e0cb0940e77c7c6683de23bc6
folarin <folarintaiye@gmail.com>
2023-04-05 13:44:43 +0100

    animations

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,18 @@
 import {Link} from "react-router-dom"
+import {useEffect} from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 export default function ProductList({product}) {
-  product.quantity=1
+  useEffect(()=>{
+    AOS.init({duration: 2000});
+  },[]);
+  // product.quantity=1
   return (
-    <div className='product-card'>
+    <div className='product-card' data-aos="zoom-in">
      
-    <Link to={`/products/${product.id}`}>
-    <img src={product.image} alt={product.name}/>
+    <Link to={`/products/${product.id}`} >
+    <img src={product.image} alt={product.name} />
     <h4> {product.title}</h4>
     <p>{product.category}</p>
     <h4 className="price"> ${product.price}</h4>
